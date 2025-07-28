415443bd8d578925e34795156816e07fbcc0b3c8
Suman maharana <sumanmaharana5555@gmail.com>
2025-07-28 22:26:42 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import 'dotenv/config';
+
+const app = express();
+const port = process.env.PORT || 4000;
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({credentials: true}));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!'); 
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
