aa02fa3788dd09de537324fb6fdace5a2dd33589
Suman maharana <sumanmaharana5555@gmail.com>
2025-08-03 23:30:48 +0530

    added verify email

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -19,7 +19,7 @@ export const register = async (req, res) => {
         const user = await User.create({name,email,password:hashed_password});
         await user.save();
 
-        const token = jwt.sign({id:user._id.toString()},process.env.JWT_SECRET,{expiresIn: '7d'});
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn: '7d'});
         res.cookie('token', token, {
             httpOnly: true,
             secure : process.env.NODE_ENV === 'production',
@@ -63,7 +63,7 @@ export const login = async(req,res)=>{
         if(!isPasswordMatch){
             return res.status(400).json({success: false, msg: "Invalid Creadintials"});
         }
-        const token = jwt.sign({id:user._id.toString()},process.env.JWT_SECRET,{expiresIn: '7d'});
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn: '7d'});
         res.cookie('token', token, {
             httpOnly: true,
             secure : process.env.NODE_ENV === 'production',
@@ -92,8 +92,69 @@ export const logout = async (req,res)=>{
         console.log(error.message);
     }
 }
+
+//Email verificaton controller - Send verification OTP to users email
+export const sendVerifyOtp = async(req,res)=>{
+    try {
+        const {userid} = req.body;
+        const user = await User.findById(userid);
+        if(!user){
+            return res.status(404).json({success: false, msg: "User not found ! Please register first"});
+        }
+        if(user.isVerified){
+            return res.status(400).json({success: false, msg: "Account already verified"});
+        }
+        //Generate six digit OTP string 
+        const  otp = Math.floor(100000 + Math.random() *900000).toString().toString();
+         user.verifyOtp = otp;
+         user.verifyOtpExpiredAt = Date.now() + 24*60*60*1000; // OTP valid for 24 hours
+         await user.save();
+         const mailOptions = {
+            from : process.env.SENDERS_EMAIL,
+            to : user.email,
+            subject : "Account Verification OTP",
+            text : `Your verification OTP is ${otp}. It is valid for 24 hours. Please do not share this OTP with anyone.` 
+         }
+         await transporter.sendMail(mailOptions);
+         res.status(200).json({success: true, msg: "Verification OTP sent successfully"});
+
+    } catch (error) {
+        return res.status(500).json({success: false, msg: "Error in sending verification OTP", error: error.message});
+        console.log(error.message);
+    }
+}
+
+export const verifyEmail = async(req,res)=>{
+    const {userId, otp} = req.body;
+
+    if(!userId || !otp) {
+        return res.status(400).json({success: false, msg: "Please provide userId and OTP"});
+    }
+    try {
+        const user = await User.findById(userId);
+        if(!user) {
+            return res.status(404).json({success: false, msg: "User not found"});
+        }
+        if(user.verifyOtp === '' || user.verifyOtp!== otp) {
+            return res.status(400).json({success: false, msg: "Invalid OTP"});
+        }
+        if(user.verifyOtpExpiredAt < Date.now()){
+            return res.status(400).json({success: false, msg: "OTP expired."});
+        }
+        user.isVerified = true;
+        user.verifyOtp = '';
+        user.verifyOtpExpiredAt = 0;
+        await user.save();
+        res.status(200).json({success: true, msg: "Email verified successfully"});    
+
+    } catch (error) {
+        res.status(500).json({success: false, msg: "Error in email verification controller", error: error.message});
+        console.log(error.message);
+    }
+}
 export default {
     register,
     login,
-    logout
+    logout,
+    sendVerifyOtp
 };
